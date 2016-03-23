d9f0218ba8e437e76b0697c2a9112dd85a969e26
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-22 22:02:07 -0400

    Addeed angular routing

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-var app = angular.module('flapperNews', []);
-app.controller('MainCtrl', [
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url:'/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+  }])
+.factory('posts', [function(){
+  var o = {
+    posts: [{title:"hello", link:'', upvotes:0}]
+  };
+  return o;
+}])
+.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
   $scope.test = 'Hello world!';
-  $scope.posts = [
-   {title: 'post 1', upvotes: 5},
-   {title: 'post 2', upvotes: 2},
-   {title: 'post 3', upvotes: 15},
-   {title: 'post 4', upvotes: 9},
-   {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === ''){return false;}
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes:4
+      upvotes:0
     });
     $scope.title = '';
     $scope.link = '';
@@ -25,5 +40,5 @@ function($scope){
 
   $scope.incrementUpvotes = function(post){
     post.upvotes += 1;
-  }
+  };
 }]);

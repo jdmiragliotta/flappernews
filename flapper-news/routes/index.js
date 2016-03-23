521bc48c079848e471b34d98465a17dce56b466c
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-03-23 00:29:07 -0400

    Routes and upvotes working

---
+++ flapper-news/routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+var mongoose = require('mongoose');
+var Post = mongoose.model("Post");
+var Comment = mongoose.model("Comment");
+
+//Gets all posts
+router.get('/posts', function(req, res, next){
+  Post.find(function(err, posts){
+    if (err) throw err;
+
+    res.json(posts);
+  })
+});
+
+//Creates a new posts
+router.post('/posts/', function(req, res, next){
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err) throw err;
+
+    res.json(post);
+  });
+});
+
+//finds post by ID
+router.param('post', function(req, res, next, id){
+  var query = Post.findById(id);
+  query.exec(function(err, post){
+    if(err) throw err;
+
+
+    req.post = post;
+    return next();
+  })
+});
+
+
+router.get('/posts/:post', function(req, res){
+  res.json(req.post);
+});
+
+//Allows upvotes
+router.put('/posts/:post/upvote', function(req, res, next){
+  req.post.upvote(function(err, post){
+    if (err) throw err;
+    res.json(post)
+  });
+});
+
+module.exports = router;
